b1dceffe96c479eaf24c611851de63d59dc8173d
trodevel <trodevel@gmail.com>
2022-11-12 03:06:25 +0100

    added replaced dates

--- sanitize.sh
+++ sanitize.sh
@@ -12,5 +12,25 @@ OUTP=$2
 #sed 's/\&amp;/\&/g' ${OUTP}.3 > ${OUTP}.4
 #sed 's/\&amp;/\&/g' ${OUTP}.4 > ${OUTP}.5
 #sed 's/\&quot;/""/g' ${OUTP}.5 > ${OUTP}.6
+cp ${OUTP}.6 ${OUTP}.7
 
-sed 's/\(Январь\|Февраль\|Март\|Апрель\|Май\|Июнь|Июль|Август|Сентябрь|Октябрь|Ноябрь|Декабрь) [0-9]+)\s\+([0-9а-я ]*)/\1/g' ${OUTP}.6 > ${OUTP}.7
+declare -A MYMAP
+MYMAP[Январь]="01"
+MYMAP[Февраль]="02"
+MYMAP[Март]="03"
+MYMAP[Апрель]="04"
+MYMAP[Май]="05"
+MYMAP[Июнь]="06"
+MYMAP[Июль]="07"
+MYMAP[Август]="08"
+MYMAP[Сентябрь]="09"
+MYMAP[Октябрь]="10"
+MYMAP[Ноябрь]="11"
+MYMAP[Декабрь]="12"
+
+for s in "${!MYMAP[@]}"
+do
+    echo "step $s"
+    val=${MYMAP[$s]}
+    sed -i "s/\(${s}\) \([0-9]\+\)/${val}-\2/g" ${OUTP}.7
+done
