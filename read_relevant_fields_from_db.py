ad91011b8c5540467ea4e9dba9a0e7d5e534ae7e
trodevel <trodevel@gmail.com>
2022-11-13 03:35:46 +0100

    added contact fields

--- read_relevant_fields_from_db.py
+++ read_relevant_fields_from_db.py
@@ -7,7 +7,11 @@ def sanitize( s ):
 import csv
 reader = csv.reader(open("db.csv", "r"))
 for row in reader:
-    print ( "{0},{1},{2},{3},{4},{5},{6}".format( row[0].replace( "https://career.habr.com/", "" ),
+    print ( "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}".format( row[0].replace( "https://career.habr.com/", "" ),
+        sanitize( row[14] ).replace( ' ', '' ),
+        sanitize( row[16] ),
+        sanitize( row[17] ),
+        sanitize( row[19] ),
         sanitize( row[29] ),
         sanitize( row[30] ),
         sanitize( row[31] ),
