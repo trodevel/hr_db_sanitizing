5984a511956935ed8531e0eaf9b4ee26bed7fce0
trodevel <trodevel@gmail.com>
2022-11-13 03:38:03 +0100

    intermediate results

--- read_relevant_fields_from_db.py
+++ read_relevant_fields_from_db.py
@@ -4,8 +4,10 @@ def sanitize( s ):
     res = s.replace( "\n", "<NL>" ).replace( '"', '""' )
     return '"' + res + '"'
 
-import csv
-reader = csv.reader(open("db.csv", "r"))
+#db_filename = "db_10.csv"       # test
+db_filename = "db.csv"          # prod
+
+reader = csv.reader(open( db_filename, "r"))
 for row in reader:
     print ( "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}".format( row[0].replace( "https://career.habr.com/", "" ),
         sanitize( row[14] ).replace( ' ', '' ),
